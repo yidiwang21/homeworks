0d4cf62add2e8696382db40f9e77923fcc4fdfae
yidiwang21 <ydwang21@gmail.com>
2019-05-08 16:04:28 -0700

    add

--- ee232/hw2/q5.m
+++ ee232/hw2/q5.m
@@ -3,9 +3,9 @@ syms alpha t
 pi = 3.14;
 T = 16.7;
 
-fun = (120*sqrt(2)*sin(2*pi/T*t))^2/100;
+fun = 288*sin(2*pi/T*t)^2;
 
-tmp = int(fun, t, alpha/(2*pi)*T, T/2) + int(fun, t, T/2+alpha/(2*pi)*T, T);
+tmp = int(fun, t, (alpha/(2*pi))*T, T/2) + int(fun, t, T/2+(alpha/(2*pi))*T, T);
 tmp = tmp/T;
 
 R_eff = 120^2/tmp;
@@ -15,3 +15,16 @@ title('R_{eff} vs. \alpha');
 
 % q = integral(@(t) fun(t)^2,alpha,T/2-alpha);
 
+%%
+clear; clc;
+syms alpha t
+T = 1;
+fun = 288*sin(2*pi/T*t)^2;
+
+tmp = int(fun, t, (alpha/(2*pi))*T, T/2) + int(fun, t, T/2+(alpha/(2*pi))*T, T);
+
+tmp = tmp/T;
+
+R_eff = 120^2/tmp;
+ezplot(R_eff, [0,pi]);
+title('R_{eff} vs. \alpha');
